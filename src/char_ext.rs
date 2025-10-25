00567cfa3ace26c49de10503bbed05671b6c4f4b
PiasekDev <63462890+PiasekDev@users.noreply.github.com>
2025-10-25 23:06:49 +0200

    perform char casing conversion tests

--- src/char_ext.rs
+++ src/char_ext.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq, Eq)]
 pub enum CharCase {
 	Upper,
 	Lower,
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use encryptr::char_ext::CharExt;
+
 fn main() {
 	println!("Hello, encryption!");
+
+	let characters = ['@', 'A', 'a', '1', '!', 'ß', 'Σ'];
+	for c in characters {
+		let uppercase = c.to_uppercase_char();
+		let lowercase = c.to_lowercase_char();
+		let case = c.case();
+		println!("Character: {}, Uppercase: {:?}, Lowercase: {:?}, Case: {:?}", c, uppercase, lowercase, case);
+	}
 }
