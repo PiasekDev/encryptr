b7b5052b83faa42f217997cb328259e6dda607f3
PiasekDev <63462890+PiasekDev@users.noreply.github.com>
2025-11-29 04:33:41 +0100

    Streamline permute method implementations to use a common helper function

--- src/extension/u8.rs
+++ src/extension/u8.rs
@@ -12,51 +12,33 @@ impl<const N: usize> PermutedBytes<N> {
 
 impl PermuteExt<6> for [u8; 48] {
 	fn permute(&self, input: &[u8]) -> PermutedBytes<6> {
-		let mut output = [0u8; 6];
-
-		for (i, &pos) in self.iter().enumerate() {
-			let b_in = (pos - 1) / 8;
-			let k_in = 7 - ((pos - 1) % 8);
-			let bit = (input[b_in as usize] >> k_in) & 1;
-			let b_out = i / 8;
-			let k_out = 7 - (i % 8);
-			output[b_out] |= bit << k_out;
-		}
-
-		PermutedBytes(output)
+		permute_impl::<6>(self, input)
 	}
 }
 
 impl PermuteExt<7> for [u8; 56] {
 	fn permute(&self, input: &[u8]) -> PermutedBytes<7> {
-		let mut output = [0u8; 7];
-
-		for (i, &pos) in self.iter().enumerate() {
-			let b_in = (pos - 1) / 8;
-			let k_in = 7 - ((pos - 1) % 8);
-			let bit = (input[b_in as usize] >> k_in) & 1;
-			let b_out = i / 8;
-			let k_out = 7 - (i % 8);
-			output[b_out] |= bit << k_out;
-		}
-
-		PermutedBytes(output)
+		permute_impl::<7>(self, input)
 	}
 }
 
 impl PermuteExt<8> for [u8; 64] {
 	fn permute(&self, input: &[u8]) -> PermutedBytes<8> {
-		let mut output = [0u8; 8];
-
-		for (i, &pos) in self.iter().enumerate() {
-			let b_in = (pos - 1) / 8;
-			let k_in = 7 - ((pos - 1) % 8);
-			let bit = (input[b_in as usize] >> k_in) & 1;
-			let b_out = i / 8;
-			let k_out = 7 - (i % 8);
-			output[b_out] |= bit << k_out;
-		}
-
-		PermutedBytes(output)
+		permute_impl::<8>(self, input)
 	}
+}
+
+fn permute_impl<const N: usize>(table: &[u8], input: &[u8]) -> PermutedBytes<N> {
+	let mut output = [0u8; N];
+
+	for (i, &pos) in table.iter().enumerate() {
+		let b_in = (pos - 1) / 8;
+		let k_in = 7 - ((pos - 1) % 8);
+		let bit = (input[b_in as usize] >> k_in) & 1;
+		let b_out = i / 8;
+		let k_out = 7 - (i % 8);
+		output[b_out] |= bit << k_out;
+	}
+
+	PermutedBytes(output)
 }
